e4613b1b7771504b393b887295910e9191256c66
RocketDev <13736845354@163.com>
2022-01-26 22:20:46 +0800

    add timeCalculator

--- Mess/lyrics.md
+++ Mess/lyrics.md
@@ -44,3 +44,45 @@ o i ko shi cha tte
 da re mo shi ra na i a shi ta e su su me  
 (ho ra) ki mi to yu me o ka sa ne te ru  
 (ho ra) so no su na ta ko no hi to mi u tsu tte ru ka ra  
+
+## 木漏れ日のエール
+
+to ri a me ni ko go e na ga ra  gyu tto hi za da i te ta da ke do
+a ki ra me so ni na tta shun kan ni  se na ka ni kan ji ta ne ga i
+u n me i no i ji wa ru
+so tto ke to ba se ru wa ta shi e to
+dai jo bu na  tte mi se ru
+ho ra fu ri so so gu tsu me ta sa mo
+i tsu ka wa ki tto ka wa tte ku yo
+na n do da tte hi ka re ru yu me to i sho na ra
+ma da ka ke nu! ke ru to chu de mo
+shin ji ra re ru yo ko no mi chi o
+**Believe to Run**
+e r!u ga ki ko e te ru ko mo re bi no yo ni
+
+na ga r!e ochi te ku shi zu ku ta chi ka na shi mi zen bu mo tte i itte ne
+ta chi a ga ru ma de no ki se tsu o  o mo ide ni su ru ta me ni
+kan tan ni i ka nai
+da ke do ni ji ga de ru ko ro ni wa
+dai jo bu tsu yo ku na re ru
+so n!u ka ru min de tsu rai kyo mo
+ma ke nai yo tte ha shi tte mi tai
+mu ne ni a ru ya ku so ku  yu ki ni kae na ga ra
+sa  su ri mu ita ko ko ro da tte
+kan ji ra re ru yo i ma mi rai o
+**Believe in Myself**
+a ta ra shi  ka ze ga ho ho so tto na !de te ku
+
+ha na re te te mo  to do ki tsu tsu ke ru
+**Believe to Run**
+**Believe to Run**
+tai yo y!o ri a tta kai ne
+ho ra fu ri so so gu tsu me ta sa mo
+i tsu ka wa ki tto ka wa tte ku yo
+nan do da tte hi ka re ru  ki mi to i sho na ra
+ma da ka ke nu ke ru to chu  de mo
+shin ji ra re ru yo ko no mi chi o
+**Believe to Run**
+e ru !ga ki ko e te ru ko mo re bi no yo ni
+
+ko ko ni a ru ko mo re bi wa zu tto
---
+++ Mess/timeCalculator.py
@@ -0,0 +1,67 @@
+class TimeStamp:
+    """
+    24小时制，以分钟为基本单位的时间戳类
+    """
+
+    __stamp: int
+
+    def checkFormat(self, time: str) -> bool:
+        """
+        true:如果是正确时间格式
+        """
+        if not (4 <= len(time) <= 5) or time.count(':') != 1:
+            return False
+        timeList = time.split(':')
+        return 0 <= int(timeList[0]) <= 24 and 0 <= int(timeList[1]) <= 60
+
+    def __init__(self, time: float | int | str, minute: int = 0) -> None:
+        """
+        e.g. TimeStamp(5.7)
+        """
+        if isinstance(time, float):
+            if time < 0 or time >= 25:
+                raise ValueError('Time is not like that!')
+            if time >= 24:
+                time -= 24
+            self.__stamp = round(time * 60)
+
+        elif isinstance(time, str):
+            if not self.checkFormat(time):
+                raise ValueError('Time is not like that!')
+            list0 = time.split(':')
+            self.__stamp = int(list0[0]) * 60 + int(list0[1])
+
+        elif isinstance(time, int):
+            if time < 0 or time > 24 or minute < 0 or minute > 60:
+                raise ValueError('Time is not like that!')
+            self.__stamp = (time if time != 24 else 0) * 60 + minute
+        else:
+            raise TypeError('Unsupported')
+
+    def __repr__(self) -> str:
+        return str(self.__stamp)
+
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, TimeStamp):
+            return False
+        if self is __o:
+            return True
+        return __o.__stamp == self.__stamp
+
+    def __sub__(self, another: object) -> int:
+        if not isinstance(another, TimeStamp):
+            raise ValueError('Can not apply two different type')
+        return self.__stamp - another.__stamp
+
+
+if __name__ == '__main__':
+    print(
+        'Input your learned times, like "15:00-17:00", end with an empty line:'
+    )
+    timeList = []
+    line = input()
+    while line != '':
+        times = line.split('-')
+        timeList.append(TimeStamp(times[1]) - TimeStamp(times[0]))
+        line = input()
+    print('You learned', sum(timeList) / 60, 'h')
